85d906e536de204ec89aa7be3eb05ffd7c049d82
Himanshuco <shimanshu0405@gmail.com>
2025-04-26 14:46:54 +0530

    Baiscs of c++

---
+++ Helloworld.cpp
@@ -0,0 +1,11 @@
+# include<iostream>
+using namespace std;
+
+//This is a comment 
+//main() - Starting point of Execution
+int main(){
+    cout<<"Hello world! I am C++"<<endl;
+    cout<<"I am Himanshu"<<endl;
+    
+
+}
